e6547043faa22464c53a286f457808dc049e7dba
ArcherDilunaire <aurelrichard00@gmail.com>
2024-03-11 17:38:23 +0100

    added scroll animation + small improvements

--- public/index.js
+++ public/index.js
@@ -1,14 +1,35 @@
+const scrollAnimateEls = document.querySelectorAll(".scroll-animate");
+const scrollTranslateEls = document.querySelectorAll(".scroll-translate");
+
 init();
 
 function init() {
     const images=["./assets/background_3.jpg","./assets/background_2.png"]
-    preload(images);
+    window.addEventListener("scroll", triggerScrollAnimation, { passive: true });
+    preload(images, () => {
+        setTimeout(() => {
+            document.querySelector("body").classList.add("loaded");
+        }, 300);
+    });
 }
-function preload(images) {
+function preload(images, callback) {
     for (var i = 0; i < images.length; i++) {
         var img=new Image();
         img.src=images[i];
+        img.onload = callback;
     }
+}
+
+
+function triggerScrollAnimation(){
+    scrollAnimateEls.forEach(el => {
+        if(el.classList.contains("animate")) return;
+        const rect = el.getBoundingClientRect();
+        const isInView = rect.top < window.innerHeight &&
+                    rect.bottom >= 0;
+        
+        if(isInView) el.classList.add("animate")
+    });
 }
 
 
